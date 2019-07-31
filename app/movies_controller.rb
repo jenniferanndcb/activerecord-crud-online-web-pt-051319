c404ee8c5cdc5c855075bbf189b8a5ec94239ef2
Jen <jenniferanndcb@gmail.com>
2019-07-31 14:15:06 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -37,11 +37,11 @@ def can_be_created_in_a_block(arg = {title: "Home Alone", release_date: "1990"})
 end
 
 def can_get_the_first_item_in_the_database
-  movie = movie.first
+  movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  movie = movie.last 
+  movie.last 
 end
 
 def can_get_size_of_the_database
