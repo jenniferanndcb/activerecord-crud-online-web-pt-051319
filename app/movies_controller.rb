620da8104fc22276d5bb61d2a3fadbabe165f233
Jen <jenniferanndcb@gmail.com>
2019-07-31 14:16:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -45,7 +45,8 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.length 
+  all_movies = Movie.all 
+  all_movies.length 
 end
 
 def can_find_the_first_item_from_the_database_using_id
